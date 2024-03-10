affb9830bf4688fe393cef50c26c53b8796b533d
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-10 13:12:51 -0300

    Atualizar configurações do projeto

--- CardapioRapido_project/settings.py
+++ CardapioRapido_project/settings.py
@@ -1,22 +1,13 @@
 from pathlib import Path
 from decouple import config, Csv
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wc*oaq#l5q_9mk7!$pszc=+%gl12rz2zcs3(p6msoctf)iqc2t'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 DJANGO_APPS = [
     'accounts',
     'core',

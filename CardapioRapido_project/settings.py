f3f44daa8db4fdbaf5463341ecab1379c634d8ad
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-12 21:38:59 -0300

    Atualiza configurações de mídia***

--- CardapioRapido_project/settings.py
+++ CardapioRapido_project/settings.py
@@ -165,8 +165,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.User'
 

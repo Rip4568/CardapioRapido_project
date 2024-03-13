bc3a0bbad5407e231d54b966b1099fef042547bd
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-12 21:39:38 -0300

    Adiciona administração de produtos com filtros e campos editáveis

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, ProductAddon
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'store', 'price', 'active')
+    list_filter = ('active', 'category', 'store')
+    search_fields = ('name', 'category', 'store')
+    list_editable = ('active',)
+    list_per_page = 30
+    list_display_links = ('name', 'category', 'store')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'category', 'store', 'price', 'active', 'image')
+        }),
+    )
+
+admin.site.register(Category)
+admin.site.register(ProductAddon)
+admin.site.register(Product, ProductAdmin)

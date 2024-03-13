67149bc1a0bab22d6c4b2d88332f75dc6a754561
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-12 21:39:06 -0300

    Adiciona URLs estáticas e importações necessárias

--- CardapioRapido_project/urls.py
+++ CardapioRapido_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,4 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('stores.api.urls_api')),
+    path('api/', include('products.api.urls_api')),
 ]
+
+# Adicione as URLs estáticas apenas no modo de desenvolvimento
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
